42ff003f5822a410a1e1ca8485c942e8bea2d5ec
vishnu kanakala <vishnu.thanos@gmail.com>
2019-08-01 21:18:56 +0530

    branch2

---
+++ vishnuDevops/Service/service.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vishnuDevops.Service
+{
+    public class service:IService
+    {
+        public string methode()
+        {
+            return "vishnu";
+        }
+    }
+}
