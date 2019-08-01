7684d799050959cb3596dfe859c28a2f242fba14
vishnu kanakala <vishnu.thanos@gmail.com>
2019-08-01 22:57:02 +0530

    new changes

---
+++ vishnuDevops/Service/Vishnu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vishnuDevops.Service
+{
+	public class Vishnu
+	{
+        public int x { set; get; }
+	}
+}
